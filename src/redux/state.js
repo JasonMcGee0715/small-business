2f0ae2c43c77c8e6d50ecfc6663d24a83f767378
JasonMcGee0715 <jdm0715@gmail.com>
2020-10-26 23:24:23 -0500

    Added initial files - components, containers, and redux.  Also, started creating the state.js

---
+++ src/redux/state.js
@@ -0,0 +1,16 @@
+export default {
+
+    user: "",
+
+    businesses: [
+        {
+            "id": 1,
+            "name": "Moonies Burger House",
+            "description": "VOTED BEST BURGER IN CEDAR PARK...12 STRAIGHT YEARS!",
+            "address": "200 S. Bell Blvd, Suite A1, Cedar Park, TX 78613",
+            "hours": "11AM - 9PM",
+            "coordsLat": 30.5087,
+            "coordsLng": -97.8225
+        }
+    ]
+}

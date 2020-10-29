ec86e46de6c3109cd157405dcb000f4f04e4fd72
JasonMcGee0715 <jdm0715@gmail.com>
2020-10-28 21:26:55 -0500

    Finished with most redux and containers.  NavBar needs tweaking as I go, but it's mostly done as well.  Starting on Listing next

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  // IconButton,
+  Typography,
+  // Button,
+} from "@material-ui/core";
+import { checkAuth } from "../../checkAuth";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appBar: {
+    paddingLeft: 25,
+    paddingRight: 25,
+    padding: 5,
+  },
+}));
+
+const NavBar = (props) => {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <AppBar position="relative" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Small Businesses of Austin
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+          <ul className="nav-list">
+            <li className="nav-list-item">
+              <Link to="/listing">Listing</Link>
+            </li>
+            {checkAuth() && (
+              <li className="nav-list-item">
+                <Link to="/add">Add</Link>
+              </li>
+            )}
+            {checkAuth() ? (
+              <li className="nav-list-item">
+                <Link to="/">Logout</Link>
+              </li>
+            ) : (
+              <li className="nav-list-item">
+                <Link to="/login">Login</Link>
+              </li>
+            )}
+          </ul>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+};
+
+export default NavBar;
